123276408c1447a893027745b5d178d561101a58
Marshall Sorenson <marshall@conceptsol.com>
2022-02-25 15:51:04 -0500

    refactor: add default CORS config

--- packages/rockets-api/src/main.ts
+++ packages/rockets-api/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(',')
+      : 'http://localhost:3000',
+  });
+
   await app.listen(3001);
 }
 bootstrap();
