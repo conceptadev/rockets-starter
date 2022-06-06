a59a9dfdc35605aa25c8e9e181b95e401b59129f
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-06 21:04:45 +0000

    chore: code standards, correct seeder

--- packages/rockets-api/src/ormconfig.ts
+++ packages/rockets-api/src/ormconfig.ts
@@ -1,54 +1,47 @@
 /**
- * !!!!! Some or all of these can be overridden by the Nest module              !!!!!!
- * !!!!! Changing these values is unlikely to have an effect on the running app !!!!!!
+ * !!!!! You MUST run build for changes in this   !!!!!!
+ * !!!!! file to take effect for all CLI commands !!!!!!
  */
 
 import { registerAs } from '@nestjs/config';
-import { TypeOrmExtOptions } from '@concepta/nestjs-typeorm-ext';
 import { ConnectionOptions } from '@jorgebodega/typeorm-seeding';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { OrgEntity } from './entities/org.entity';
 
-const dbSSL =
-  'string' === typeof process.env.DATABASE_SSL
-    ? process.env.DATABASE_SSL === 'true'
-    : process.env.DATABASE_SSL || false;
+export const ormDefaultConfig = (): TypeOrmModuleOptions &
+  Partial<ConnectionOptions> => {
+  // return the configuration
+  return {
+    type: 'postgres',
+    url:
+      process.env.DATABASE_URL ??
+      'postgresql://postgres:postgres@rockets-starter-postgres:5432/postgres',
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) ?? false,
+    entities: [UserEntity, OrgEntity],
+    subscribers: [__dirname + '/**/*.subscriber.js'],
+    seeders: [__dirname + '/**/*.seeder.js'],
+    defaultSeeder: 'RootSeeder',
+    migrations: [__dirname + '/migrations/*.js'],
+    cli: {
+      migrationsDir: './src/migrations',
+    },
+    extra: {
+      ssl:
+        process.env?.DATABASE_SSL === 'true'
+          ? {
+              rejectUnauthorized: false,
+            }
+          : false,
+    },
+    logging: 'all',
+  };
+};
 
-const defaultUrl =
-  'postgresql://postgres:postgres@localhost:5432/rockets-starter';
+// import this into your Nest app
+export const ormConfig = registerAs('TYPEORM_MODULE_CONFIG', ormDefaultConfig);
 
-export const ormDefaultConfig: TypeOrmModuleOptions &
-  Partial<ConnectionOptions> = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL || defaultUrl,
-  synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) || false,
-  entities: [UserEntity, OrgEntity],
-  subscribers: [__dirname + '/**/*.subscriber.js'],
-  seeders: [__dirname + '/**/*.seeder.js'],
-  defaultSeeder: 'RootSeeder',
-  migrations: [__dirname + '/migrations/*.js'],
-  cli: {
-    migrationsDir: './src/migrations',
-  },
-  extra: {
-    ssl: dbSSL
-      ? {
-          rejectUnauthorized: false,
-        }
-      : false,
-  },
-  logging: 'all',
-} as TypeOrmExtOptions;
+const defaultConfig = ormDefaultConfig();
 
-export const ormConfig = registerAs(
-  'TYPEORM_MODULE_CONFIG',
-  (): TypeOrmModuleOptions & Partial<ConnectionOptions> =>
-    ({
-      ...ormDefaultConfig,
-      url: process.env.DATABASE_URL || defaultUrl,
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) || false,
-    } as TypeOrmModuleOptions & Partial<ConnectionOptions>),
-);
-
-export default ormDefaultConfig;
+// this is used by the CLI tools
+export default defaultConfig;
