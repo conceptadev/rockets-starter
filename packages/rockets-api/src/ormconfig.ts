f64df441f2be40f53009b69657f44ccdb18ca1da
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-07-11 15:19:59 +0000

    fix: call dotenv.config() for typeorm cli

--- packages/rockets-api/src/ormconfig.ts
+++ packages/rockets-api/src/ormconfig.ts
@@ -3,8 +3,15 @@
  * !!!!! file to take effect for all CLI commands !!!!!!
  */
 
+import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import { ormConfigFactory } from './config/typeorm.config';
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!! This config() call is for CLI ONLY         !!!
+// !!! DO NOT MOVE THIS TO THE ormConfigFactory() !!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+config();
 
 const options = {
   ...ormConfigFactory(),
