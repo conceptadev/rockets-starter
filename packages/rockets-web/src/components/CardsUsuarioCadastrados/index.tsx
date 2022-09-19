24fc9d2117bf5d5aec1318c170d939814d77c093
Isaque menezes <isaquemenezes91@gmail.com>
2022-09-18 21:06:00 -0300

    modal

--- packages/rockets-web/src/components/CardsUsuarioCadastrados/index.tsx
+++ packages/rockets-web/src/components/CardsUsuarioCadastrados/index.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.css';
 
 import user2 from '../../assets/imgs/Vector (10).svg';
+import Modal from "../modal/index";
 
 export default function CardUsuario(){
+    const [showPostModal, setShowPostModal] = useState(false);
+    
+
+    function togglePostModal(){
+        setShowPostModal(!showPostModal)
+        
+        
+    }
+
     return(
         
             <div className="container-card">
-                <div className="cards">
-                    <img src={user2} alt="foto-perfil" className="foto-perfil-card"/>
-                    <h2 className="card-nome">Lorem Impsum</h2>
-                    <h2 className="card-idade">25 anos</h2>
-                    <h2 className="card-telefone">83 9888-8888</h2>
-                    <h2 className="card-tec">Angular, React, Nest JS</h2>
-                    <h2 className="card-atuacao">Front-End</h2>
+                <button onClick={()=>togglePostModal()}>
+                    <div className="cards">
+                        <img src={user2} alt="foto-perfil" className="foto-perfil-card"/>
+                        <h2 className="card-nome">Lorem Impsum</h2>
+                        <h2 className="card-idade">25 anos</h2>
+                        <h2 className="card-telefone">83 9888-8888</h2>
+                        <h2 className="card-tec">Angular, React, Nest JS</h2>
+                        <h2 className="card-atuacao">Front-End</h2>
 
-                </div>
+                    </div>
+                </button>
+                {showPostModal &&(
+                    <Modal close={togglePostModal}/>
+                )
+                }
             </div>
         
     );
