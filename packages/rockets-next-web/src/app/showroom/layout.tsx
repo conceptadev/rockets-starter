891c2c5edf0d69980267bff5d8674893ad97ca67
Rodrigo Dias <rodrigo.elfo@gmail.com>
2023-09-11 10:45:53 -0300

    starter next - add table

---
+++ packages/rockets-next-web/src/app/showroom/layout.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { ReactNode } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import ContainerWithDrawer from "@/components/ContainerWithDrawer/ContainerWithDrawer";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import BusinessOutlinedIcon from "@mui/icons-material/BusinessOutlined";
+import GroupsOutlinedIcon from "@mui/icons-material/GroupsOutlined";
+import PaymentsOutlinedIcon from "@mui/icons-material/PaymentsOutlined";
+import ExtensionOutlinedIcon from "@mui/icons-material/ExtensionOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ColorLensOutlinedIcon from "@mui/icons-material/ColorLensOutlined";
+import GridOnIcon from "@mui/icons-material/GridOn";
+import CodeIcon from "@mui/icons-material/Code";
+
+export default function Showroom({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const drawerMenuItems = [
+    {
+      id: "/showroom/profile",
+      icon: <PersonOutlineOutlinedIcon />,
+      text: "Profile",
+      onClick: () => router.push("/profile"),
+    },
+    {
+      id: "/showroom/account",
+      icon: <BusinessOutlinedIcon />,
+      text: "Account",
+      onClick: () => alert("foo"),
+      active: true,
+    },
+    {
+      id: "/showroom/billing",
+      icon: <PaymentsOutlinedIcon />,
+      text: "Billing",
+      onClick: () => alert("foo"),
+    },
+    {
+      id: "/showroom/integrations",
+      icon: <ExtensionOutlinedIcon />,
+      text: "Integrations",
+      onClick: () => alert("foo"),
+    },
+    {
+      id: "/showroom/home",
+      icon: <HomeOutlinedIcon />,
+      text: "Home",
+      onClick: () => router.push("/home"),
+    },
+    {
+      id: "/showroom/table",
+      icon: <GridOnIcon />,
+      text: "Table",
+      onClick: () => router.push("/table"),
+    },
+    {
+      id: "/showroom/teamMembers",
+      icon: <GroupsOutlinedIcon />,
+      text: "TeamMembers",
+      onClick: () => router.push("/team-members"),
+    },
+    {
+      id: "/showroom/jsonform",
+      icon: <CodeIcon />,
+      text: "Jsonform",
+      onClick: () => router.push("/jsonform"),
+    },
+    {
+      id: "/showroom/simpleform",
+      icon: <CodeIcon />,
+      text: "Simple Forms",
+      onClick: () => router.push("/simple-forms"),
+    },
+    {
+      id: "/showroom/theme",
+      icon: <ColorLensOutlinedIcon />,
+      text: "Theme",
+      onClick: () => router.push("/theme"),
+    },
+    {
+      id: "login",
+      icon: <PersonOutlineOutlinedIcon />,
+      text: "Login",
+      onClick: () => router.push("/login"),
+    },
+    {
+      id: "signUp",
+      icon: <PersonOutlineOutlinedIcon />,
+      text: "SignUp",
+      onClick: () => router.push("/sign-up"),
+    },
+  ];
+
+  return (
+    <ContainerWithDrawer
+      key={pathname}
+      drawerItems={drawerMenuItems}
+      currentId={pathname}
+      logo="/logo.svg"
+      showNotifications
+      notificationsNumber={6}
+      notificationsOnClick={() => console.log("click")}
+      avatar="https://source.unsplash.com/random"
+      text="John Smith"
+      subText="Amazing Inc."
+    >
+      {children}
+    </ContainerWithDrawer>
+  );
+}
---
+++ packages/rockets-next-web/src/app/showroom/table/CustomRowOptions.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import { CustomRowOptionsProps } from '@concepta/react-material-ui/dist/components/Table/Table';
+import Settings from '@mui/icons-material/Settings';
+
+const CustomRowOptions: FC<CustomRowOptionsProps> = ({ close }) => {
+  const handleMenuClick = (log: string) => () => {
+    close();
+    return log;
+  };
+
+  return (
+    <>
+      <MenuItem onClick={handleMenuClick('Settings')}>
+        <Settings />
+      </MenuItem>
+      <MenuItem onClick={handleMenuClick('Edit')}>Edit</MenuItem>
+      <MenuItem onClick={handleMenuClick('Open')}>Open</MenuItem>
+    </>
+  );
+};
+
+export default CustomRowOptions;
