8f95942d708a1bdb0733a7c1b5567a3412f1bc01
Rodrigo Dias <rodrigo.elfo@gmail.com>
2023-09-11 11:18:00 -0300

    starter next - add jsonforms

---
+++ packages/rockets-next-web/src/app/showroom/jsonform/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { FC } from "react";
+import { Text } from "@concepta/react-material-ui";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import ArrayForm from "./ArrayForm";
+import ArrayMultiForm from "./ArrayMultiForm";
+import SimpleForm from "./SimpleForm";
+import OtherFormElements from "./OtherFormElements";
+import SwitchElementForm from "./SwitchElementForm";
+
+const Jsonform: FC = () => (
+  <Container maxWidth="xs">
+    <Text
+      variant="h4"
+      fontFamily="Inter"
+      fontSize={30}
+      fontWeight={800}
+      mt={1}
+      gutterBottom
+    >
+      Forms
+    </Text>
+
+    <SimpleForm />
+
+    <Divider sx={{ my: 8 }} />
+
+    <ArrayForm />
+
+    <Divider sx={{ my: 8 }} />
+
+    <OtherFormElements />
+
+    <Divider sx={{ my: 8 }} />
+
+    <SwitchElementForm />
+
+    <ArrayForm />
+
+    <ArrayMultiForm />
+  </Container>
+);
+
+export default Jsonform;
---
+++ packages/rockets-next-web/src/utils/emailValidation/emailValidation.ts
@@ -0,0 +1,8 @@
+const emailValidation = (input: string) => {
+  const email =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return email.test(input);
+};
+
+export default emailValidation;
