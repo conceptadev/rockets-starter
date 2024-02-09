b5dff430d7f365d2271ab0137a3b136e23eeecc3
Jefferson Silva <brnmouras@gmail.com>
2024-02-09 01:25:49 -0300

    refactor: add handlers for success and error requests

--- packages/rockets-next-web/src/app/(auth)/reset-password/page.tsx
+++ packages/rockets-next-web/src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
 import { AuthModule } from "@concepta/react-material-ui";
+import { toast } from "react-toastify";
+
+interface NetworkError {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
 
 const ResetPassword = () => {
   return (
-    <AuthModule route="resetPassword" moduleProps={{ signInPath: "/login" }} />
+    <AuthModule
+      route="resetPassword"
+      moduleProps={{
+        signInPath: "/login",
+        onSuccess: () => toast.success("Success!"),
+        onError: (error) =>
+          toast.error(
+            (error as NetworkError)?.response?.data?.message ||
+              "An error has occurred. Please try again later or contact support for assistance."
+          ),
+      }}
+    />
   );
 };
 
