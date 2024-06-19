44f1a72e424c1a92ce6c8ae57b4e22899e31c25f
Thiago Ramalho <thiago.nramalho@gmail.com>
2024-06-19 16:15:13 -0300

    chore: add exception filter

--- packages/rockets-api/src/main.ts
+++ packages/rockets-api/src/main.ts
@@ -1,6 +1,7 @@
 import { AppModule } from './app.module';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { SwaggerUiService } from '@concepta/nestjs-swagger-ui';
+import { ExceptionsFilter } from '@concepta/nestjs-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,9 @@ async function bootstrap() {
   const swaggerUiService = app.get(SwaggerUiService);
   swaggerUiService.builder().addBearerAuth();
   swaggerUiService.setup(app);
+
+  const exceptionsFilter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new ExceptionsFilter(exceptionsFilter));
 
   // set up cors
   app.enableCors({
