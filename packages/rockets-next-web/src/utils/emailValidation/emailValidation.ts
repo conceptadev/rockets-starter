d9d1973621f4cb705b7b45f1662d4db788523677
Jefferson Silva <brnmouras@gmail.com>
2024-08-21 18:05:17 -0300

    refactor: remove unused functions

--- packages/rockets-next-web/src/utils/emailValidation/emailValidation.ts
+++
@@ -1,8 +0,0 @@
-const emailValidation = (input: string) => {
-  const email =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return email.test(input);
-};
-
-export default emailValidation;
